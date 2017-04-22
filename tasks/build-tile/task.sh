828011135c6cef6526543b31b05ed586bccd017d
Josh Ghiloni <jghiloni@ecsteam.com>
2017-04-21 20:03:53 -0600

    Add build-tile

---
+++ tasks/build-tile/task.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -x
+
+BASEDIR=`pwd`
+
+BOSH_RELEASE_DIR=${BASEDIR}/bosh-release-binary
+TILE_SOURCE_DIR=${BASEDIR}/tile-source
+VERSION_DIR=${BASEDIR}/version
+
+OUTPUT_DIR=${BASEDIR}/build-tile-output
+
+CURRENT_VERSION=`cat ${VERSION_DIR}/version`
+
+cp ${BOSH_RELEASE_DIR}/influxdb-nozzle-bosh-release-$(cat ${BOSH_RELEASE_DIR}/tag).tgz \
+	${TILE_SOURCE_DIR}/resources/influxdb-nozzle-bosh-release.tgz
+
+cd ${TILE_SOURCE_DIR}
+tile build ${CURRENT_VERSION}
+
+cp product/ecsteam-influxdb-${CURRENT_VERSION}.pivotal ${OUTPUT_DIR}
