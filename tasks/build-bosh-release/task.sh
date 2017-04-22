333ca2f9f2423afdda08d6a14279c50dc8e1a99e
Josh Ghiloni <jghiloni@ecsteam.com>
2017-04-21 19:06:58 -0600

    Fix errors in bosh script

--- tasks/build-bosh-release/task.sh
+++ tasks/build-bosh-release/task.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -13,10 +13,18 @@ curl -o ${basedir}/cf-linux-amd64.tgz 'http://cli.run.pivotal.io/stable?release=
 
 cd ${basedir}/bosh-release-source
 
+cat <<EOF > config/private.yml
+---
+blobstore_secret: ${BLOBSTORE_SECRET}
+blobstore:
+  local:
+    blobstore_path: /tmp/influxdb-nozzle-bosh-release
+EOF
+
 bosh add blob ${basedir}/cf-linux-amd64.tgz cf_cli
 bosh add blob ${basedir}/nozzle-binary/influxdb-nozzle.jar influxdb_nozzle
 
-bosh upload blobs
-bosh create release --force --final --with-tarball --version ${num}
+bosh -n upload blobs
+bosh -n create release --force --final --with-tarball --version ${num}
 
 cp releases/influxdb-nozzle-bosh-release/influxdb-nozzle-bosh-release-${num}.tgz ${basedir}/build-bosh-release-output
